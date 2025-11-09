504fd10eec6361b8ed8d88847597fadf4ccf56dd
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-11-09 18:20:11 +0100

    swsw

--- js/script.js
+++ js/script.js
@@ -1,50 +1,20 @@
-// 1
-let celsius = 1;
-let fahrenheit = (celsius * 9/5) + 32;
-console.log("Температура в Фаренгейтах: ", fahrenheit);
+const slider = document.querySelector(".slider__input");
+const image = document.querySelector(".slider__image");
 
-// 2
-let daysInMonth = 30;
-let hoursInMonth = daysInMonth * 24;
-let minutesInMonth = hoursInMonth * 60;
-console.log("Годин у місяці: ", hoursInMonth, "Хвилин у місяці: ", minutesInMonth);
+const resizeImage = _.debounce((value) => {
+  image.style.width = value + "%";
+}, 150);
 
-// 3
-let health = 100;
-let energy = 80;
-health -= 20;
-energy -= 30;
-console.log("Рівень здоров'я: ", health, "Рівень енергії: ", energy);
+slider.addEventListener("input", (e) => {
+  resizeImage(e.target.value);
+});
 
-// 4
-let purchaseAmount = 500;
-let discount = purchaseAmount * 0.10;
-let discountedAmount = purchaseAmount - discount;
-console.log("Сума після знижки: ",discountedAmount);
+const box = document.getElementById("box");
 
-// 5
-let floatNumber = 7.8;
-let roundedDown = Math.floor(floatNumber);
-console.log("Округлене число: ",roundedDown);
+const moveBox = _.debounce((x, y) => {
+  box.style.transform = `translate(${x - 25}px, ${y - 25}px)`;
+}, 100);
 
-// 6
-let floatString = "12.34";
-let parsedFloat = parseFloat(floatString);
-console.log("Десяткове число: ", parsedFloat);
-
-// 7
-let intString = "42";
-let parsedInt = parseInt(intString);
-console.log("Ціле число: ", parsedInt);
-
-// 8
-let number = 64;
-let sqrtNumber = Math.sqrt(number);
-console.log("Квадратний корінь: ", sqrtNumber);
-
-// 9. Перетворення рядка у ціле число та назад у рядок
-let integerNumber = 123;
-let stringNumber = "456";
-let convertedToInt = parseInt(stringNumber);
-let convertedToString = integerNumber.toString();
-console.log("Перетворене у число: ", convertedToInt, "Перетворене у рядок: ", convertedToString);
+document.addEventListener("mousemove", (e) => {
+  moveBox(e.clientX, e.clientY);
+});

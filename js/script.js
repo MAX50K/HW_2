2bc68d96a89d16e3fb71037a1bb17b6b97a8d3dc
MAX50K <146106579+MAX50K@users.noreply.github.com>
2026-01-13 16:35:48 +0100

    e

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const slider = document.querySelector(".slider__input");
 const image = document.querySelector(".slider__image");
+const box = document.getElementById("box");
 
 let timeout;
 
@@ -10,3 +11,10 @@ slider.addEventListener("input", (e) => {
     image.style.width = e.target.value + "%";
   }, 200);
 });
+
+const moveBox = _.debounce((event) => {
+  box.style.left = event.clientX + "px";
+  box.style.top = event.clientY + "px";
+}, 100);
+
+document.addEventListener("mousemove", moveBox);

11e6c393049d3bac24ebf42a9574319985319acf
MAX50K <146106579+MAX50K@users.noreply.github.com>
2026-01-13 17:08:23 +0100

    sws

--- js/script.js
+++ js/script.js
@@ -2,19 +2,16 @@ const slider = document.querySelector(".slider__input");
 const image = document.querySelector(".slider__image");
 const box = document.getElementById("box");
 
-let timeout;
-
-slider.addEventListener("input", (e) => {
-  clearTimeout(timeout);
-
-  timeout = setTimeout(() => {
-    image.style.width = e.target.value + "%";
-  }, 200);
-});
+const updateImageSize = _.debounce((value) => {
+  image.style.width = value * 3 + "px";
+}, 150);
 
 const moveBox = _.debounce((event) => {
   box.style.left = event.clientX + "px";
   box.style.top = event.clientY + "px";
 }, 100);
 
+slider.addEventListener("input", (e) => {
+  updateImageSize(e.target.value);
+});
 document.addEventListener("mousemove", moveBox);
